18a1d027ccb26776df23957dc4b42d0b8262146a
Pranaya Raghubanshi <praghubanshi12@gmail.com>
2022-06-14 19:05:44 +0545

    attendance sql updated with data

--- backend/attendance.sql
+++ backend/attendance.sql
@@ -1,129 +1,156 @@
--- phpMyAdmin SQL Dump
--- version 5.1.0
--- https://www.phpmyadmin.net/
+-- MariaDB dump 10.19  Distrib 10.4.24-MariaDB, for Win64 (AMD64)
 --
--- Host: 127.0.0.1
--- Generation Time: Jun 14, 2022 at 07:21 AM
--- Server version: 10.4.18-MariaDB
--- PHP Version: 7.4.16
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
+-- Host: localhost    Database: attendance
+-- ------------------------------------------------------
+-- Server version	10.4.24-MariaDB
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `attendance`
---
-
-DELIMITER $$
---
--- Functions
---
-CREATE DEFINER=`root`@`localhost` FUNCTION `fn_dateFormat` (`date` DATETIME) RETURNS VARCHAR(25) CHARSET utf8mb4 BEGIN DECLARE sql_var varchar(1000); set sql_var = (SELECT DATE_FORMAT(date, '%Y-%m-%d') AS CONVERTED_DATETIME); return sql_var; END$$
-
-CREATE DEFINER=`root`@`localhost` FUNCTION `fn_dateTimeFormat` (`date` DATETIME) RETURNS VARCHAR(25) CHARSET utf8mb4 BEGIN
-DECLARE sql_var varchar(1000);
-set sql_var = (SELECT DATE_FORMAT(date, '%Y-%m-%d %h:%i:%s %p') AS CONVERTED_DATETIME);
-return sql_var;
-END$$
-
-DELIMITER ;
-
--- --------------------------------------------------------
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
 -- Table structure for table `attendance_logs`
 --
 
+DROP TABLE IF EXISTS `attendance_logs`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `attendance_logs` (
-  `log_id` int(11) NOT NULL,
+  `log_id` int(11) NOT NULL AUTO_INCREMENT,
   `employee_id` int(11) NOT NULL,
   `in_time` datetime DEFAULT NULL,
-  `out_time` datetime DEFAULT NULL
+  `out_time` datetime DEFAULT NULL,
+  PRIMARY KEY (`log_id`),
+  KEY `fk_employees_attendance_logs` (`employee_id`),
+  CONSTRAINT `fk_employees_attendance_logs` FOREIGN KEY (`employee_id`) REFERENCES `employees` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- --------------------------------------------------------
+--
+-- Dumping data for table `attendance_logs`
+--
+
+LOCK TABLES `attendance_logs` WRITE;
+/*!40000 ALTER TABLE `attendance_logs` DISABLE KEYS */;
+/*!40000 ALTER TABLE `attendance_logs` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `departments`
 --
 
+DROP TABLE IF EXISTS `departments`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `departments` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `department_name` varchar(100) DEFAULT NULL,
   `created_at` datetime NOT NULL,
   `created_by` int(11) NOT NULL,
   `updated_at` datetime DEFAULT NULL,
-  `updated_by` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `updated_by` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `departments`
 --
 
-INSERT INTO `departments` (`id`, `department_name`, `created_at`, `created_by`, `updated_at`, `updated_by`) VALUES
-(1, 'Administrator123', '2022-05-26 08:41:19', 1, NULL, NULL),
-(2, 'Information Technology', '2022-05-26 08:41:30', 1, NULL, NULL),
-(3, 'Accounts', '2022-05-26 08:43:01', 1, NULL, NULL),
-(4, 'Human Resources', '2022-05-26 08:44:51', 1, NULL, NULL),
-(5, 'Quality Assurance', '2022-05-26 08:46:08', 1, NULL, NULL),
-(6, 'Compliance 123', '2022-05-26 08:47:16', 1, NULL, NULL),
-(7, 'Security', '2022-05-28 19:50:51', 1, NULL, NULL),
-(8, 'Treasury', '2022-05-28 20:48:42', 1, NULL, NULL);
-
--- --------------------------------------------------------
+LOCK TABLES `departments` WRITE;
+/*!40000 ALTER TABLE `departments` DISABLE KEYS */;
+INSERT INTO `departments` VALUES (1,'Administrator123','2022-05-26 08:41:19',1,NULL,NULL),(2,'Information Technology','2022-05-26 08:41:30',1,NULL,NULL),(3,'Accounts','2022-05-26 08:43:01',1,NULL,NULL),(4,'Human Resources','2022-05-26 08:44:51',1,NULL,NULL),(5,'Quality Assurance','2022-05-26 08:46:08',1,NULL,NULL),(6,'Compliance 123','2022-05-26 08:47:16',1,NULL,NULL),(7,'Security','2022-05-28 19:50:51',1,NULL,NULL),(8,'Treasury','2022-05-28 20:48:42',1,NULL,NULL);
+/*!40000 ALTER TABLE `departments` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `employees`
 --
 
+DROP TABLE IF EXISTS `employees`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `employees` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `shift_id` int(11) NOT NULL,
   `department_id` int(11) NOT NULL,
   `role_id` int(11) NOT NULL,
   `full_name` varchar(100) NOT NULL,
   `email_id` varchar(40) NOT NULL,
-  `password` varchar(20) NOT NULL,
+  `password` varchar(200) NOT NULL,
   `dob` date DEFAULT NULL,
   `contact_no` varchar(15) NOT NULL,
   `address` varchar(100) NOT NULL,
   `created_at` datetime NOT NULL,
   `created_by` int(11) NOT NULL,
   `updated_at` datetime DEFAULT NULL,
-  `updated_by` datetime DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `updated_by` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email_id` (`email_id`),
+  KEY `fk_employee_department` (`department_id`),
+  KEY `fk_employees_shifts` (`shift_id`),
+  KEY `fk_employee_role` (`role_id`),
+  CONSTRAINT `fk_employee_department` FOREIGN KEY (`department_id`) REFERENCES `departments` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `fk_employee_role` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `fk_employees_shifts` FOREIGN KEY (`shift_id`) REFERENCES `shifts` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- --------------------------------------------------------
+--
+-- Dumping data for table `employees`
+--
+
+LOCK TABLES `employees` WRITE;
+/*!40000 ALTER TABLE `employees` DISABLE KEYS */;
+INSERT INTO `employees` VALUES (1,6,1,1,'Admin','admin@gmail.com','$2a$12$9dtKdyswziBZilKJox260.EwPu.hQBXC3rK2s4cwj4w15NqBsFxlG','2000-06-01','9813123456','Satdobato','2022-06-14 13:45:28',1,NULL,NULL),(2,6,2,5,'Pranaya Raghubanshi','praghubanshi12@gmail.com','$2a$08$UA0zUDBo6Qc/ocepRcTS5OuAEKCr4XocYBsTTK4Mrp/9sEEtq9zdu','1997-06-29','9813815037','Kupondole, Lalitpur','2022-06-14 17:35:50',1,NULL,NULL);
+/*!40000 ALTER TABLE `employees` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `roles`
 --
 
+DROP TABLE IF EXISTS `roles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `roles` (
-  `id` int(11) NOT NULL,
-  `parent_id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `parent_id` int(11) DEFAULT NULL,
   `role_name` varchar(30) NOT NULL,
   `created_at` datetime NOT NULL,
   `created_by` int(11) NOT NULL,
   `updated_at` datetime DEFAULT NULL,
-  `updated_by` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `updated_by` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- --------------------------------------------------------
+--
+-- Dumping data for table `roles`
+--
+
+LOCK TABLES `roles` WRITE;
+/*!40000 ALTER TABLE `roles` DISABLE KEYS */;
+INSERT INTO `roles` VALUES (1,0,'Admin','2022-06-14 14:41:45',1,NULL,NULL),(2,0,'HR','2022-06-14 14:47:45',1,NULL,NULL),(3,0,'Manager','2022-06-14 14:49:05',1,NULL,NULL),(4,3,'Supervisor','2022-06-14 14:49:48',1,NULL,NULL),(5,4,'Employee','2022-06-14 14:55:46',1,NULL,NULL);
+/*!40000 ALTER TABLE `roles` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `shifts`
 --
 
+DROP TABLE IF EXISTS `shifts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `shifts` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `shift_name` enum('Regular','Morning','Evening','Night') NOT NULL,
   `start_week_day` enum('Sunday','Monday') NOT NULL,
   `allow_overtime` tinyint(1) DEFAULT NULL,
@@ -131,152 +158,58 @@ CREATE TABLE `shifts` (
   `created_at` datetime NOT NULL,
   `created_by` smallint(6) NOT NULL,
   `updated_at` datetime DEFAULT NULL,
-  `updated_by` smallint(6) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `updated_by` smallint(6) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `shifts`
 --
 
-INSERT INTO `shifts` (`id`, `shift_name`, `start_week_day`, `allow_overtime`, `start_overtime`, `created_at`, `created_by`, `updated_at`, `updated_by`) VALUES
-(6, 'Regular', 'Sunday', 1, 2, '2022-05-25 23:44:04', 1, NULL, NULL),
-(7, 'Morning', 'Monday', 0, 1, '2022-05-25 23:49:16', 1, NULL, NULL),
-(8, 'Evening', 'Sunday', 1, 3, '2022-05-25 23:50:24', 1, NULL, NULL),
-(9, 'Night', 'Monday', 1, 2, '2022-05-25 23:50:56', 1, NULL, NULL);
-
--- --------------------------------------------------------
+LOCK TABLES `shifts` WRITE;
+/*!40000 ALTER TABLE `shifts` DISABLE KEYS */;
+INSERT INTO `shifts` VALUES (6,'Regular','Sunday',1,2,'2022-05-25 23:44:04',1,NULL,NULL),(7,'Morning','Monday',0,1,'2022-05-25 23:49:16',1,NULL,NULL),(8,'Evening','Sunday',1,3,'2022-05-25 23:50:24',1,NULL,NULL),(9,'Night','Monday',1,2,'2022-05-25 23:50:56',1,NULL,NULL);
+/*!40000 ALTER TABLE `shifts` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `working_days`
 --
 
+DROP TABLE IF EXISTS `working_days`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `working_days` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `working_day` varchar(10) NOT NULL,
   `start_time` varchar(10) NOT NULL,
   `end_time` varchar(10) NOT NULL,
   `created_at` datetime NOT NULL,
   `created_by` smallint(6) NOT NULL,
   `updated_at` datetime DEFAULT NULL,
-  `updated_by` smallint(6) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `updated_by` smallint(6) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `working_days`
 --
 
-INSERT INTO `working_days` (`id`, `working_day`, `start_time`, `end_time`, `created_at`, `created_by`, `updated_at`, `updated_by`) VALUES
-(1, 'Sunday', '9:30 AM', '6:00 PM', '2022-05-27 17:28:22', 1, NULL, NULL),
-(2, 'Monday', '9:00 AM', '6:00 PM', '2022-05-27 17:29:43', 1, NULL, NULL),
-(3, 'Tuesday', '9:30 AM', '06:30 PM', '2022-05-27 17:30:13', 1, NULL, NULL),
-(4, 'Wednesday', '9:30 AM', '05:30 PM', '2022-05-27 17:30:33', 1, NULL, NULL),
-(5, 'Thursday', '9:30 AM', '6:00 PM', '2022-05-27 17:30:59', 1, NULL, NULL),
-(6, 'Friday', '9:30 AM', '2:30 PM', '2022-05-27 17:31:23', 1, NULL, NULL);
+LOCK TABLES `working_days` WRITE;
+/*!40000 ALTER TABLE `working_days` DISABLE KEYS */;
+INSERT INTO `working_days` VALUES (1,'Sunday','9:30 AM','6:00 PM','2022-05-27 17:28:22',1,NULL,NULL),(2,'Monday','9:00 AM','6:00 PM','2022-05-27 17:29:43',1,NULL,NULL),(3,'Tuesday','9:30 AM','06:30 PM','2022-05-27 17:30:13',1,NULL,NULL),(4,'Wednesday','9:30 AM','05:30 PM','2022-05-27 17:30:33',1,NULL,NULL),(5,'Thursday','9:30 AM','6:00 PM','2022-05-27 17:30:59',1,NULL,NULL),(6,'Friday','9:30 AM','2:30 PM','2022-05-27 17:31:23',1,NULL,NULL);
+/*!40000 ALTER TABLE `working_days` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `attendance_logs`
---
-ALTER TABLE `attendance_logs`
-  ADD PRIMARY KEY (`log_id`),
-  ADD KEY `fk_employees_attendance_logs` (`employee_id`);
-
---
--- Indexes for table `departments`
---
-ALTER TABLE `departments`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `employees`
---
-ALTER TABLE `employees`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `email_id` (`email_id`),
-  ADD KEY `fk_employee_department` (`department_id`),
-  ADD KEY `fk_employees_shifts` (`shift_id`),
-  ADD KEY `fk_employee_role` (`role_id`);
-
---
--- Indexes for table `roles`
---
-ALTER TABLE `roles`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `shifts`
---
-ALTER TABLE `shifts`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `working_days`
---
-ALTER TABLE `working_days`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `attendance_logs`
---
-ALTER TABLE `attendance_logs`
-  MODIFY `log_id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `departments`
---
-ALTER TABLE `departments`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
-
---
--- AUTO_INCREMENT for table `employees`
---
-ALTER TABLE `employees`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `roles`
---
-ALTER TABLE `roles`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `shifts`
---
-ALTER TABLE `shifts`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
-
---
--- AUTO_INCREMENT for table `working_days`
---
-ALTER TABLE `working_days`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `attendance_logs`
---
-ALTER TABLE `attendance_logs`
-  ADD CONSTRAINT `fk_employees_attendance_logs` FOREIGN KEY (`employee_id`) REFERENCES `employees` (`id`) ON DELETE CASCADE;
-
---
--- Constraints for table `employees`
---
-ALTER TABLE `employees`
-  ADD CONSTRAINT `fk_employee_department` FOREIGN KEY (`department_id`) REFERENCES `departments` (`id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `fk_employee_role` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `fk_employees_shifts` FOREIGN KEY (`shift_id`) REFERENCES `shifts` (`id`) ON DELETE CASCADE;
-COMMIT;
-
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-06-14 19:03:02
